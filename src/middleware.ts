bf801b6a8ecd106ecc7d5affa7b28a9436a72479
swasthik-007 <mohanty.swastik4@gmail.com>
2025-03-29 19:50:56 +0530

    PP

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
+  "/",
+  "/privacypolicy",
   "/api/clerk/webhook(.*)",
   "/sign-in(.*)",
   "/sign-up(.*)",
