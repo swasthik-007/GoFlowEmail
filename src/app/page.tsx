7d92817e23a9540d1d7d03cea7dc99e1ef7da1cc
swasthik-007 <mohanty.swastik4@gmail.com>
2025-03-29 20:44:50 +0530

    PP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-// import LinkAccountButton from "./_components/link-account-button";
-
+import LinkAccountButton from "./_components/link-account-button";
+import Link from "next/link";
 export default async function Home() {
   return (
     <>
-      {/* <LinkAccountButton /> */}
-      <h1>helloo bhayyy</h1>
+      <LinkAccountButton />
+      {/* <h1>helloo bhayyy</h1> */}
+      <footer className="mt-6">
+        <Link href="/privacypolicy" className="text-blue-500 underline">
+          Privacy Policy
+        </Link>
+      </footer>
     </>
   );
 }
