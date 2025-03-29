02886b5240f91cf027b7fca56db9b8bbcd907728
swasthik-007 <mohanty.swastik4@gmail.com>
2025-03-29 21:13:52 +0530

    PP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
-import LinkAccountButton from "./_components/link-account-button";
-import Link from "next/link";
+// import LinkAccountButton from "./_components/link-account-button";
+// import Link from "next/link";
 export default async function Home() {
   return (
     <>
-      <LinkAccountButton />
+      {/* <LinkAccountButton />
       {/* <h1>helloo bhayyy</h1> */}
-      <footer className="mt-6">
+      {/* <footer className="mt-6">
         <Link href="/privacypolicy" className="text-blue-500 underline">
           Privacy Policy
         </Link>
-      </footer>
+      </footer> */}
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <div className="p-6">
+          <h1 className="text-xl font-bold">Welcome to GoFloww Atoms</h1>
+          {/* Your homepage content */}
+        </div>
+
+        {/* Footer with privacy policy link */}
+        <footer className="bg-gray-100 p-4 text-center">
+          <a href="/privacy-policy" className="text-blue-600 underline">
+            Privacy Policy
+          </a>
+        </footer>
+      </div>
     </>
   );
 }
